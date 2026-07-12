58456c5581cd10e7d00377d24fb7293728f43ed3
Pierre <pierre28bbt@gmail.com>
2026-07-12 18:44:12 +0200

    mail wip

--- ossif-backend/src/config/mailer.ts
+++ ossif-backend/src/config/mailer.ts
@@ -1,5 +1,9 @@
 import nodemailer from 'nodemailer';
 import 'dotenv/config';
+import dns from 'dns';
+
+// Force Node.js to prefer IPv4 over IPv6 globally for DNS resolutions
+dns.setDefaultResultOrder('ipv4first');
 
 console.log('--- Debug Mailer Config ---');
 console.log('SMTP_EMAIL:', process.env.SMTP_EMAIL);
