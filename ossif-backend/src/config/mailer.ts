a5b338e64bda2caa1e91d9b7773f4ae2559e6f3b
Pierre <pierre28bbt@gmail.com>
2026-07-12 17:44:54 +0200

    wip

--- ossif-backend/src/config/mailer.ts
+++ ossif-backend/src/config/mailer.ts
@@ -1,6 +1,11 @@
 import nodemailer from 'nodemailer';
 import 'dotenv/config';
 
+console.log('--- Debug Mailer Config ---');
+console.log('SMTP_EMAIL:', process.env.SMTP_EMAIL);
+console.log('SMTP_PASSWORD loaded?', !!process.env.SMTP_PASSWORD);
+console.log('---------------------------');
+
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
